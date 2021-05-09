3ba8bf80e4837188461ea8dd236fbff0cf780e4d
Paolo Gomez <78209142+pattherogue@users.noreply.github.com>
2021-05-09 15:54:48 -0700

    Alert Banner JS

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const tasks = 0;
 //html for the banner
 alertBanner.innerHTML = 
 `<div class="alert-banner">
-<p><strong>Alert:</strong> You have <strong>6</srong> overdue tasks
- to complete</p>
- <p class="alert-banner-close">&times;</p>
- </div>`
+    <p><strong>Alert:</strong> You have <strong>${tasks}</srong> unread messages</p>
+    <p class="alert-banner-close"> &times;</p>
+    </div>`
 
  alertBanner.addEventListener('click', e => {
      const element = e.target;
